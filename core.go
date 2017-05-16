a8f42a90de0d7da6dde3831d8de17843bfe55db9
agoalofalife <agoalofalife@gmail.com>
2017-05-16 21:56:13 +0300

    add excecute in action read so prepare

--- core.go
+++ core.go
@@ -1,32 +1,24 @@
 package bpm
 
-import (
-	"log"
-	"os"
-)
-
 type Core struct {
 	collection string
-	Actions    map[string]Action
+	cookie     Cookie
 }
 
 // Init application
 func Start(collection string) *Core {
-	mapsAction := make(map[string]Action)
 	core := Core{}
 	core.collection = collection + "Collection"
-	mapsAction["read"] = Read()
-	core.Actions = mapsAction
 	return &core
 }
+
 // return link Read Action
-func (core Core ) Read()  *Select {
-	return Read()
+func (core *Core) Read() *Select {
+	return Read(core)
 }
 
 func BmpTest() {
-	temp := Read()
-	temp.FilterConstructor("Id eq guid'68800b54-0e46-4388-b9f3-cbb45df42364'")
-	log.Println(temp.Execute())
-	os.Exit(2)
+	//temp.FilterConstructor("Id eq guid'68800b54-0e46-4388-b9f3-cbb45df42364'")
+	//log.Println(temp.Execute())
+	//os.Exit(2)
 }
--- select.go
+++ select.go
@@ -1,37 +1,45 @@
 package bpm
 
 import (
-//"github.com/andelf/go-curl"
+	//"net/url"
+	"github.com/andelf/go-curl"
+	"log"
+	"os"
 )
 
 type Select struct {
 	method string
 	url    string
+	core   *Core
 }
 
 // init action Read or Select
-func Read() *Select {
+func Read(core *Core) *Select {
 	read := Select{}
 	read.method = `GET`
+	read.core = core
 	return &read
 }
 
 func (read *Select) Execute() bool {
-	//parameters := url.QueryEscape(read.url)
-	//easy := curl.EasyInit()
-	//defer easy.Cleanup()
-	//if easy != nil {
-	//	easy.Setopt(curl.OPT_URL, url)
-	//	easy.Setopt(curl.OPT_VERBOSE, true)
-	//	easy.Setopt(curl.OPT_COOKIEJAR, c.fileCookie)
-	//	easy.Setopt(curl.OPT_HTTPHEADER, []string{"POST  HTTP/1.0", "Content-type: application/json"})
-	//	easy.Setopt(curl.OPT_POSTFIELDS, string(jsonString))
-	//}
-	//if error := easy.Perform(); error != nil {
-	//	err = error.Error()
-	//} else {
-	//	state = true
-	//}
+
+	prepareUrl := read.core.collection + read.url
+	//prepareUrl    := read.core.collection +  url.QueryEscape(read.url)
+	urlHome, _ := Config().String("auth.UrlHome")
+	urlHome += prepareUrl
+
+	easy := curl.EasyInit()
+	defer easy.Cleanup()
+	if easy != nil {
+		easy.Setopt(curl.OPT_URL, urlHome)
+		easy.Setopt(curl.OPT_VERBOSE, true)
+		easy.Setopt(curl.OPT_COOKIEJAR, read.core.cookie.fileCookie)
+		easy.Setopt(curl.OPT_HTTPHEADER, []string{read.method + "  HTTP/1.0", "Content-type: application/json"})
+	}
+	if error := easy.Perform(); error != nil {
+		log.Println(error)
+		os.Exit(2)
+	}
 	return true
 }
 
