d90a725e8c5e904d704fcdaba33fe991cb4cb525
agoalofalife <agoalofalife@gmail.com>
2017-05-20 10:05:51 +0300

    json set header in habdler

--- JsonHandler.go
+++ JsonHandler.go
@@ -5,6 +5,8 @@ import (
 )
 type Json struct {
 	fields map[string]string
+	accept string
+	contentType string
 }
 
 
@@ -13,6 +15,16 @@ func JsonInit() DataType {
 	return &json
 }
 
+// get content type for header request
+func (json Json) getContentType() (contentType string)  {
+	return  json.contentType
+}
+
+// get accept in header request
+func (json Json) getAccept() (accept string) {
+	return json.accept
+}
+// handler
 func (json Json) Handler() (map[string]string, error) {
 	maps := make(map[string]string)
 	error := errors.New("")
